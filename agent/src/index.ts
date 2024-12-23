17286692dab05e485d16fce8a84f234e187b1c82
Johan Schaap <jschaap@Johans-MacBook-Air.local>
2024-12-23 09:04:13 +0100

    configuring agent

--- agent/src/index.ts
+++ agent/src/index.ts
@@ -61,6 +61,8 @@ import path from "path";
 import { fileURLToPath } from "url";
 import yargs from "yargs";
 
+// import { mainCharacter } from '../mainCharacter.ts'
+
 const __filename = fileURLToPath(import.meta.url); // get the resolved path to the file
 const __dirname = path.dirname(__filename); // get the name of the directory
 
